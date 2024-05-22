abbb68ef88a92a1d54ba9435ea9cdf289640f07a
AleksandrFomenko <136353457+AleksandrFomenko@users.noreply.github.com>
2024-05-22 14:13:07 +0300

    Update copying_staircase_finishes.py

--- MGE/copying_staircase_finishes.py
+++ MGE/copying_staircase_finishes.py
@@ -3,14 +3,16 @@ import System
 clr.AddReference('RevitAPI')
 clr.AddReference("RevitServices")
 from Autodesk.Revit import DB
-from Autodesk.Revit.DB import DirectShape as ds
+from Autodesk.Revit.DB import DirectShape as DS
 from System.Collections.Generic import List
 from Autodesk.Revit.DB import FilteredElementCollector as FEC
 
-class units:
+
+class Units:
     def __init__(self):
         pass
-    def _set_value_to_par(self,item,parameter_name,value):
+
+    def set_value_to_param(self, item, parameter_name, value):
         par = item.LookupParameter(parameter_name)
         if par and not par.IsReadOnly:
             if par.StorageType == DB.StorageType.String:
@@ -22,14 +24,15 @@ class units:
             if par.StorageType == DB.StorageType.Double:
                 par.Set(value)
                 return
+
     def return_geom(self, item):
         opt = DB.Options()
         lt = List[DB.GeometryObject]()
         geometry_item = item.get_Geometry(opt)
         for geom_obj in geometry_item:
             if isinstance(geom_obj, DB.GeometryInstance):
-                instanceGeom = geom_obj.GetInstanceGeometry()
-                geom_list = [geom for geom in instanceGeom if isinstance(geom, DB.Solid) and geom.Volume > 0]
+                instance_geom = geom_obj.GetInstanceGeometry()
+                geom_list = [geom for geom in instance_geom if isinstance(geom, DB.Solid) and geom.Volume > 0]
                 if len(geom_list) == 1:
                     for geom in geom_list:
                         lt.Add(geom)
@@ -38,52 +41,53 @@ class units:
                     for geom in geom_list:
                         if geom != largest_geom:
                             lt.Add(geom)
-                    
         return lt
-    def create_shape(self,category,lt):
-        direct_shape = ds.CreateElement(doc,category)
+
+    def create_shape(self, category, lt):
+        direct_shape = DS.CreateElement(doc, category)
         direct_shape.SetShape(lt)
 
 
-class stairs:
-    #Категория модели в контексте
+class Stairs:
+    # Категория модели в контексте
     cat_id = DB.ElementId(DB.BuiltInCategory.OST_Floors)
-    un = units()
-    #Площадки
-    _landings_ = (
-        FEC(doc).
-        OfCategory(DB.BuiltInCategory.OST_StairsLandings).
-        WhereElementIsNotElementType().
-        ToElements()
+    un = Units()
+
+    # Площадки
+    landings = (
+        FEC(doc)
+        .OfCategory(DB.BuiltInCategory.OST_StairsLandings)
+        .WhereElementIsNotElementType()
+        .ToElements()
     )
-    #Марши
-    _runs_ = (
-        FEC(doc).
-        OfCategory(DB.BuiltInCategory.OST_StairsRuns).
-        WhereElementIsNotElementType().
-        ToElements()
+
+    # Марши
+    runs = (
+        FEC(doc)
+        .OfCategory(DB.BuiltInCategory.OST_StairsRuns)
+        .WhereElementIsNotElementType()
+        .ToElements()
     )
+
     def __init__(self):
         pass
-    def create_shape_langings(self):
-        for land in stairs._landings_:
+
+    def create_shape_landings(self):
+        for land in Stairs.landings:
             geom = self.un.return_geom(land)
-            self.un.create_shape(self.cat_id,geom)
+            self.un.create_shape(self.cat_id, geom)
+
     def create_shape_runs(self):
-        for land in stairs._runs_:
-            geom = self.un.return_geom(land)
-            self.un.create_shape(self.cat_id,geom)
-    
+        for run in Stairs.runs:
+            geom = self.un.return_geom(run)
+            self.un.create_shape(self.cat_id, geom)
 
 
-
-
-with DB.Transaction(doc,"qwe") as t:
+with DB.Transaction(doc, "Create Shapes") as t:
     t.Start()
 
-    st = stairs()
-    st.create_shape_langings()
+    st = Stairs()
+    st.create_shape_landings()
     st.create_shape_runs()
 
     t.Commit()
-
